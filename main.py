03430b84a002867aee95caa5b28a75f543d48696
madhavan nk <147168558+madhavannkp@users.noreply.github.com>
2025-06-13 16:12:46 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+
+# Name ="madhavan"
+# age ="23"
+# role ="Software engineer" 
+
+# print("i am " +Name +" working as a " +role +" may age is "+age)
+
+# Name ="madhavan"
+# age =23
+# role ="Software engineer" 
+
+# print(f"I am {Name}, working as a {role}, my age is {age}")
+
+# a="Good"
+# b="Morning"
+# c=a+" "+b
+# print(c)
+
+
+# a="Good"
+# b="Morning"
+# c=f"hi {a} {b}"
+# print(c)
+
