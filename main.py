8f89f569aec1dc7910a63ab0f0d9f2ada35bd132
madhavan nk <147168558+madhavannkp@users.noreply.github.com>
2025-06-16 16:22:20 +0530

    third commit

--- main.py
+++ main.py
@@ -22,3 +22,100 @@
 # c=f"hi {a} {b}"
 # print(c)
 
+# a = 10
+# b = 3
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a//b)
+# print(a**b)
+
+
+# import math
+
+
+# print("Square root of 25:", math.sqrt(25))
+
+# print("2 to the power 3:", math.pow(2, 3))
+
+# print("Floor of 3.7:", math.floor(3.7))
+
+# print("Ceil of 3.7:", math.ceil(3.7))
+
+# print("Absolute of -7:", math.fabs(-7))
+
+# print("Factorial of 5:", math.factorial(5))
+
+# print("Pi value:", math.pi)
+
+# print("Euler's number:", math.e)
+
+
+
+# import random
+
+
+# print("Random float:", random.random())
+
+
+# print("Random integer:", random.randint(1, 100))
+
+
+# print("Random float between 5 and 10:", random.uniform(5, 10))
+
+
+# fruits = ['apple', 'banana', 'cherry', 'orange']
+# print("Random fruit:", random.choice(fruits))
+
+
+# print("Sample 2 fruits:", random.sample(fruits, 2))
+
+
+# print("Choices 3 fruits (with repeat):", random.choices(fruits, k=3))
+
+
+# numbers = [1, 2, 3, 4, 5]
+# random.shuffle(numbers)
+# print("Shuffled numbers:", numbers)
+
+
+
+# x = 10
+
+# if x > 5:
+#     print("x is greater than 5")
+
+
+# x = 3
+
+# if x > 5:
+#     print("x is greater than 5")
+# else:
+#     print("x is 5 or less")
+
+
+# x = 3
+
+# if x > 10:
+#     print("x is greater than 10")
+# elif x > 5:
+#     print("x is greater than 5 but less than or equal to 10")
+# else:
+#     print("x is 5 or less")
+
+
+# age = 20
+
+# result = "Adult" if age >= 18 else "Minor"
+# print(result)
+
+# x = 10
+
+# if x > 5:
+#     if x < 15:
+#         print("x is between 5 and 15")
+
+
